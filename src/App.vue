04c747c1640b6b12537340945148b2a151ad5241
Luca <lucafagiolini@live.it>
2024-02-28 14:26:40 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<!-- ********** JS ********** -->
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  components: {
+    AppTitle
+  }
+
+}
+</script>
+
+
+
+
+
+<!-- ********** HTML ********** -->
+<template>
+  <div>
+    <AppTitle />
+  </div>
+</template>
+
+
+
+
+<!-- ********** CSS ********** -->    
+<style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<template>
+    <div>
+        <h1>"La mia prima app con Vite!"</h1>
+    </div>
+</template>
+
+<style scoped>
+template {
+    display: flex;
+    justify-content: center;
+}
+
+h1 {
+    text-align: center;
+    color: red;
+}
+</style>
